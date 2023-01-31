9cdce3218cbbf3f0f72d68eba997f8f9ec3f7ee0
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-01-31 10:50:31 +0300

    add register user function

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -3,12 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Record } from '../interfaces/record.iterface';
+import { Tokens } from '../interfaces/tokens.interface';
 
 @Injectable()
 export class HttpService {
   private URL = 'http://localhost:8000';
 
   constructor(private http: HttpClient) {}
+
+  public registerUser = (login: string, password: string, confirmedPassword: string): Observable<Tokens> =>
+    this.http.post<Tokens>(this.URL + '/auth/registration', {
+      login: login,
+      password: password,
+      confirmedPassword: confirmedPassword,
+    });
 
   public getAllRecords = (headers: HttpHeaders): Observable<Record[]> =>
     this.http.get<Record[]>(this.URL + '/records', {
