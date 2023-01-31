068cb9ca8c83d43c80797ef0c786c9cf948c8e18
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-01-31 14:15:09 +0300

    init registration component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 import { AuthService } from './services/auth.service';
 import { HttpService } from './services/http.service';
@@ -9,5 +9,5 @@ import { HttpService } from './services/http.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private httpService: HttpService, private authService: AuthService) {}
+  constructor(private httpService: HttpService, @Inject(AuthService) private authService: AuthService) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RegistrationComponent } from './components/registration/registration.component';
 import { AuthService } from './services/auth.service';
 import { HttpService } from './services/http.service';
 import { TokenStorageService } from './services/token.service';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegistrationComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [HttpService, AuthService, TokenStorageService],
   bootstrap: [AppComponent],
