ecf2bd7a67ee9891dd6fd3f721679b553d7f21ae
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-02-01 10:53:57 +0300

    login component layout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,4 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public login = '';
+  public password = '';
+}
