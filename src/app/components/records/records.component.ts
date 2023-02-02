e4e4517de613b94b2a9c65e41452b66499e4fd0a
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-02-02 10:15:16 +0300

    error handling when no connection with server

--- src/app/components/records/records.component.ts
+++ src/app/components/records/records.component.ts
@@ -1,7 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { catchError, throwError } from 'rxjs';
 
 import { Doctor } from '../../interfaces/doctor.interface';
 import { HttpService } from '../../services/http.service';
+import { SnackBarService } from '../../services/snack-bar.service';
 
 @Component({
   selector: 'app-records',
@@ -9,8 +13,21 @@ import { HttpService } from '../../services/http.service';
   styleUrls: ['./records.component.css'],
 })
 export class RecordsComponent {
-  constructor(private httpService: HttpService) {
-    httpService.getAllDoctors().subscribe((result) => (this.doctors = result));
+  constructor(private httpService: HttpService, private snackBarService: SnackBarService) {
+    httpService
+      .getAllDoctors()
+      .pipe(
+        catchError((errorResponse: HttpErrorResponse) => {
+          if (errorResponse.status === 0) {
+            this.snackBarService.showErrorSnack(this.snackBarService.NO_CONNECTION);
+            return throwError(() => new Error(this.snackBarService.NO_CONNECTION));
+          }
+
+          this.snackBarService.showErrorSnack(errorResponse.error.errors.map((e: { msg: string }) => e.msg).join('. '));
+          return throwError(() => new Error(errorResponse.status.toString()));
+        })
+      )
+      .subscribe((result) => (this.doctors = result));
   }
 
   public doctors: Doctor[] = [];
