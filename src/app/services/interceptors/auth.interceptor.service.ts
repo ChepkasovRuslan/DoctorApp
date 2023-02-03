d665df88a1881e3ed2c8610c64ed7ddac25aa1a4
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2023-02-03 16:54:05 +0300

    add auto refresh token

--- src/app/services/interceptors/auth.interceptor.service.ts
+++ src/app/services/interceptors/auth.interceptor.service.ts
@@ -3,19 +3,32 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, Observable, throwError } from 'rxjs';
 
+import { AuthService } from '../auth.service';
 import { TokenStorageService } from '../token.service';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private tokenStorageService: TokenStorageService, private router: Router) {}
+  constructor(
+    private tokenStorageService: TokenStorageService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.tokenStorageService.getToken();
 
     if (token) {
-      request = request.clone({
-        setHeaders: { Authorization: token },
-      });
+      if (this.tokenExpired(token)) {
+        const refreshToken = this.tokenStorageService.getRefreshToken();
+        this.authService.refreshToken(refreshToken!!).subscribe((result) => {
+          this.tokenStorageService.saveToken(result.accessToken);
+          this.tokenStorageService.saveRefreshToken(result.refreshToken);
+        });
+      } else {
+        request = request.clone({
+          setHeaders: { Authorization: token },
+        });
+      }
     }
 
     return next.handle(request).pipe(
@@ -28,5 +41,10 @@ export class AuthInterceptorService implements HttpInterceptor {
         return throwError(err);
       })
     );
+  }
+
+  private tokenExpired(token: string) {
+    const expiry = JSON.parse(atob(token.split('.')[1])).exp;
+    return Math.floor(new Date().getTime() / 1000) >= expiry;
   }
 }
